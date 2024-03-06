af8f046eb307dbc59d5cba6b21dba92cab776b7e
shubham Rawat <rawatshubam269@gmail.com>
2024-03-06 11:55:38 +0530

    Datatypes

--- all_In_One_JS.js
+++ all_In_One_JS.js
@@ -1,3 +1,10 @@
+/* ====
+   NOTE: 
+   1. Prefer not to use var as it cause issue in block and functional scope
+   2. Code Readability should be high which reflect the experience level of developer.
+   ====
+*/
+
 const name = "Shubham";
 let age = 25;
 var totalMarks = 899;
@@ -8,9 +15,4 @@ totalMarks = 900;
 
 // we can use table to display values in table format when have multiple values
 
-/* ====
-   NOTE: Prefer not to use var as it cause issue in block and functional scope
-   ====
-*/
-
-console.table({ name, age, totalMarks });
+// console.table({ name, age, totalMarks });
