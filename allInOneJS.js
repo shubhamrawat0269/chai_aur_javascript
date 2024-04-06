2b6c22c386fc611103024451fd918a869d20b6a6
shubham Rawat <rawatshubam269@gmail.com>
2024-04-06 13:43:20 +0530

    Chapter 4 ____ Variables

--- allInOneJS.js
+++ allInOneJS.js
@@ -1,1 +1,17 @@
-console.log("Shubham Rawat");
+/* Variables */
+
+const userId = 100091;
+let userName = "Shubham Rawat";
+var userAge = 29;
+
+/* Note --> It's not preferable to declare and assign value to a variable like below. */
+userProfession = "Web Developer";
+
+/* Please Note --> const variables can't be modified */
+// userId = 100092;
+
+userName = "Abhishek Rawat";
+userAge = 22;
+userProfession = "Chemist";
+
+console.table({ userId, userName, userAge, userProfession });
