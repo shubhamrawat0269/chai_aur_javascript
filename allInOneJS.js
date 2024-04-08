3eac9ffa9e63d7502d6795e1632c6814d66eb669
shubham Rawat <rawatshubam269@gmail.com>
2024-04-08 09:16:58 +0530

    Chapter 2. Datatypes

--- allInOneJS.js
+++ allInOneJS.js
@@ -1,4 +1,4 @@
-/* Variables */
+/* +++++++++++++++ Variables +++++++++++++++ */
 
 const userId = 100091;
 let userName = "Shubham Rawat";
@@ -15,3 +15,34 @@ userAge = 22;
 userProfession = "Chemist";
 
 console.table({ userId, userName, userAge, userProfession });
+
+/* +++++++++++++++ Datatypes and ECMA +++++++++++++++ */
+
+// "use strict" // treat all js code to newer version
+
+// alert(3+3); // we are using node js , not browser
+
+console.log(3 + 3); // code readability & future proof of code must be high
+
+// console.log("Shubham");
+
+let name = "Shubham Rawat";
+let age = 26;
+let isLoogedIn = false;
+let state;
+
+// ---------- Primitive type ----------
+// number -> 2 ^ 53
+// bigint
+// string -> ""
+// boolean -> t/f
+// null -> standalone value
+// undefined
+// symbol
+
+// ---------- Non-Primitive ( Reference ) type ----------
+
+// object
+
+// console.log(typeof undefined);
+// console.log(typeof null);
